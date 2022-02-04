fd26a49fee8b0151079f5f2c9c9ed350b8329557
Vlad Mastjulins <42850622+Vlad2335@users.noreply.github.com>
2022-02-04 11:00:03 +0100

    Aanpassingen 05/02/22 - 07/02/22Delete HtmlParser.cs
    Unnecessary file

--- SignatureAssignmentV2/Parsers/cs_to_html.cs
+++ SignatureAssignmentV2/Parsers/cs_to_html.cs
@@ -10,68 +10,101 @@ namespace SignatureAssignmentV2
         public void HtmlSite()
         {
 
-            XmlTextReader xtr = new XmlTextReader("C:\\Users\\vlad.mastjulins\\source\\repos\\SignatureAssignmentV2\\SignatureAssignmentV2\\signature_data.xml");
+            XmlTextReader xtr = new XmlTextReader(@"C:\Users\vlad.mastjulins\source\repos\SignatureAssignmentV2\SignatureAssignmentV2\XML\signature_data.xml");
 
-            var file = XDocument.Load("C:\\Users\\vlad.mastjulins\\source\\repos\\SignatureAssignmentV2\\SignatureAssignmentV2\\signature_data.xml");
+            var file = XDocument.Load(@"C:\Users\vlad.mastjulins\source\repos\SignatureAssignmentV2\SignatureAssignmentV2\XML\signature_data.xml");
+
 
             var info = from signature in file.Root?.Descendants("row")
                        select new
                        {
-                           Name = signature.Element("name").Value,
-                           job = signature.Element("job_title").Value,
-                           department = signature.Element("department").Value,
-                           company = signature.Element("company").Value,
-                           companyAdres = signature.Element("company_adres").Value,
-                           telNumber = signature.Element("tel_nummer").Value,
-                           email = signature.Element("email_adres").Value,
-                           companySite = signature.Element("company_website").Value
-                        };
+                           Name = signature?.Element("name")?.Value,
+                           styleName = signature?.Element("name")?.Attribute("styleName")?.Value, // Responsible for the style of the name
 
-            string html = "<table cellpadding='5' cellspacing='0' style='border: 1px solid #ccc;font-size: 9pt;font-family:calibri;>#"; 
+
+                           image = signature?.Element("image")?.Value,
+                           imageStyle = signature?.Element("image")?.Attribute("imageStyle")?.Value, // Responsible for the style of imageStyle (Height & Width)
+                           imageStyle1 = signature?.Element("image")?.Attribute("imageStyle2")?.Value, // Redundant, needs fix soon
+                           Src = signature?.Element("image")?.Attribute("src")?.Value, // Responsible for reading Base64 images
+
+
+                           job = signature?.Element("job_title")?.Value,
+                           styleJob = signature?.Element("job_title")?.Attribute("styleJob")?.Value, // Responsible for the style of job_title
+
+
+                           department = signature?.Element("department")?.Value,
+                           styleDept = signature?.Element("department")?.Attribute("styleDept")?.Value, // Responsible for the style of department
+
+
+                           company = signature?.Element("company")?.Value,
+                           styleComp = signature?.Element("company")?.Attribute("styleComp")?.Value, // Responsible for the style of company 
+
+
+                           companyAdres = signature?.Element("company_adres")?.Value,
+                           styleCompAdr = signature?.Element("company_adres")?.Attribute("styleCompAdr")?.Value, // Responsible for the style of job_title
+
+
+                           telNumber = signature?.Element("tel_nummer")?.Value,
+                           styleNumb = signature?.Element("tel_number")?.Attribute("styleNumb")?.Value, // Responsible for the style of tel_number
+
+
+                           email = signature?.Element("email_adres")?.Value,
+                           styleMail = signature?.Element("email_adres")?.Attribute("styleMail")?.Value, // Responsible for the style of email_adres
+
+
+                           companySite = signature?.Element("company_website")?.Value,
+                           styleCompWeb = signature?.Element("company_website")?.Attribute("styleCompWeb")?.Value // Responsible for the style of company_website
+
+
+                       };
+            
+            string html = "<html>";
 
             foreach (var item in info) {
 
-               // html += "<table cellpadding='5' cellspacing='0' style='border: 1px solid #ccc;font-size: 9pt;font-family:calibri;>#"; // Finish this code
+                html += "<body> # <table cellpadding='5' cellspacing='0'>";
 
                 html += "<tr>";
-                html += "<th style='background-color: #B8D8FD;border: 1px solid #ccc'>" + item.Name + "</th>#";
+                html += "<td style='" + item.imageStyle1 + "'>" + "<img style='" + item.imageStyle + "' src=" + item.Src + "></img></td>#"; // Prints out image.
                 html += "</tr>";
 
                 html += "<tr>";
-                html += "<th style='width:120px;border: 1px solid #ccc'>" + item.job + "</td>#";
+                    html += "<td style='" + item.styleName + "'>" + item.Name + "</td>#";
                 html += "</tr>";
 
                 html += "<tr>";
-                html += "<th style='width:120px;border: 1px solid #ccc'>" + item.department + "</td>#";
+                    html += "<td style='" + item.styleJob + "'>" + item.job + "</td>#";
                 html += "</tr>";
 
                 html += "<tr>";
-                html += "<th style='width:120px;border: 1px solid #ccc'>" + item.company + "</td>#";
+                    html += $"<td style='" + item.styleDept + "'>" + item.department + "</td>#";
                 html += "</tr>";
 
                 html += "<tr>";
-                html += "<th style='width:120px;border: 1px solid #ccc'>" + item.companyAdres + "</td>#";
+                    html += "<td style='" + item.styleComp + "'>" + item.company + "</td>#";
                 html += "</tr>";
 
                 html += "<tr>";
-                html += "<th style='width:120px;border: 1px solid #ccc'>" + item.email + "</td>#";
+                    html += "<td style='" + item.styleCompAdr + "'>" + item.companyAdres + "</td>#";
                 html += "</tr>";
 
                 html += "<tr>";
-                html += "<th style='width:120px;border: 1px solid #ccc'>" + item.companySite + "</td>#";
+                    html += "<td style='" + item.styleMail + "'>" + item.email + "</td>#";
+                html += "</tr>";
+
+                html += "<tr>";
+                    html += "<td style='" + item.styleCompWeb  + "'>" + item.companySite + "</td>#";
                 html += "</tr>";
 
 
-                // html += "</table>";
-
+                html += "</table> # </body> # </html>";
 
             }
 
-            html += "</table>#</body>#</html>";
+ 
             html = html.Replace("#", Environment.NewLine);
 
-
-            // HTML File Generator
+            /* ---------------------------------------------- HTML File Generator --------------------------------*/
 
             // If directory doesn't exist create it
             if (!Directory.Exists(@"C:\Users\vlad.mastjulins\source\repos\SignatureAssignmentV2\SignatureAssignmentV2\OutputFiles"))
@@ -81,10 +114,6 @@ namespace SignatureAssignmentV2
             // File.WriteAllText(Path.Combine(outputDirPath, generatedFilename), htmlStructure);
 
 
-            /*
-             File.WriteAllText(@"C:\Users\vlad.mastjulins\source\repos\Signature Assignment\Signature Assignment\test.htm", html);
-             Console.WriteLine("HTML File Created.");
-            */
 
             Console.ReadLine();
             
--- SignatureAssignmentV2/Program.cs
+++ SignatureAssignmentV2/Program.cs
@@ -2,6 +2,8 @@
 using System.IO;
 using System.Xml;
 using System.Xml.Linq;
+using System.Text;
+using static System.Console;
 
 namespace SignatureAssignmentV2
 {
@@ -10,26 +12,40 @@ namespace SignatureAssignmentV2
         static void Main(string[] args)
         {
 
-            XmlTextReader xtr = new XmlTextReader(@"C:\Users\vlad.mastjulins\source\repos\SignatureAssignmentV2\SignatureAssignmentV2\signature_data.xml");
+            XmlTextReader xtr = new XmlTextReader(@"C:\Users\vlad.mastjulins\source\repos\SignatureAssignmentV2\SignatureAssignmentV2\XML\signature_data.xml");
 
-            var file = XDocument.Load(@"C:\Users\vlad.mastjulins\source\repos\SignatureAssignmentV2\SignatureAssignmentV2\signature_data.xml");
+            var file = XDocument.Load(@"C:\Users\vlad.mastjulins\source\repos\SignatureAssignmentV2\SignatureAssignmentV2\XML\signature_data.xml");
 
             var info = from signature in file.Root?.Descendants("row")
                        select new
                        {
-                           Name = signature.Element("name").Value,
-                           job = signature.Element("job_title").Value,
-                           department = signature.Element("department").Value,
-                           company = signature.Element("company").Value,
-                           companyAdres = signature.Element("company_adres").Value,
-                           telNumber = signature.Element("tel_nummer").Value,
-                           email = signature.Element("email_adres").Value,
-                           companySite = signature.Element("company_website").Value
+                           Name = signature?.Element("name")?.Value,
+
+                           image = signature?.Element("image")?.Value,
+
+                           job = signature?.Element("job_title")?.Value,
+
+                           department = signature?.Element("department")?.Value,
+
+                           company = signature?.Element("company")?.Value,
+
+                           companyAdres = signature?.Element("company_adres")?.Value,
+
+                           telNumber = signature?.Element("tel_nummer")?.Value,
+
+                           email = signature?.Element("email_adres")?.Value,
+
+                           companySite = signature?.Element("company_website")?.Value
                        };
+
+
+
 
             foreach (var item in info)
             {
+                
                 Console.WriteLine(item.Name);
+                Console.WriteLine(item.image);
                 Console.WriteLine(item.job);
                 Console.WriteLine(item.department);
                 Console.WriteLine(item.company);
@@ -41,8 +57,10 @@ namespace SignatureAssignmentV2
             }
 
 
-            Cs_to_html cls = new Cs_to_html();
+            Cs_to_html cls = new Cs_to_html(); // Links cs_to_html.cs to program.cs (Also Line 61)
             cls.HtmlSite();
+
+
 
         }
     }
